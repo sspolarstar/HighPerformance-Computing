5bf7e8a1e353c3fa37acbfd107fc53849dd811a4
sspolarstar <47253708+sspolarstar@users.noreply.github.com>
2025-02-20 09:26:57 -0700

    Update countSort.cpp

--- Homework-3/countSort.cpp
+++ Homework-3/countSort.cpp
@@ -3,50 +3,78 @@
 #include <string>
 #include <thread>
 #include <omp.h>
-#include <algorithm>
-#include <iostream>
 #include <stdio.h>
 
-void count_sort( int a[], int n) {
-    int i, j, count;
-    int * temp = static_cast<int*>(malloc(n* sizeof ( int )));
+// 1. If we try to parallelize the for i loop (the outer loop),\
+ which variables should be private and which should be shared? (5 points)
+    //Answer: the variables j, and count should be private while \
+    temp and 'a' should be shared. j and count would both provide \
+    race conditions if they weren't unique to the thread.
+
+// 2. If we consider the memcpy implementation not thread-safe,\
+ how would you approach parallelizing this operation? (5 points)
+    //Answer: create a memory safe version of the function. Using an OMP parallel for loop\
+    to copy the elements from temp to a.
     
-    for (i = 0; i < n; i++) {
-        count = 0;
-        for (j = 0; j < n; j++)
-        if (a[j] < a[i]){
-            count++;
+// 3. Write a C/C++ OpenMP program that includes a parallel implementation\
+ of count_sort. 30 points)
+
+
+    void count_sort_thread_safe( int a[], int n) {
+        int i, j, count;
+        int * temp = static_cast<int*>(malloc(n* sizeof ( int )));
+                
+        #pragma omp parallel for private(count, j) //prevent race condition on threads 
+        for (i = 0; i < n; i++) {//loop as we were given, but with braces.
+            count = 0;
+            for (j = 0; j < n; j++){ 
+                if (a[j] < a[i]){
+                    count++;
+                }
+                else if(a[j] == a[i] && j < i){
+                    count++;
+                }
+            }
+            temp[count] = a[i];
         }
-        else if(a[j] == a[i] && j < i){
-            count++;
-        }
-        temp[count] = a[i];
-    }
-    memcpy(a, temp, n*sizeof(int)); //NOT THREAD SAFE
-    free(temp);
-  } /* count_sort */
+
+        //thread safe 'memcpy'
+        #pragma omp parallel for //No calculated values, so no need for private variables
+            for (int i_o = 0; i_o < n; i_o++) {
+                a[i_o] = temp[i_o]; //copy the values (input) number of threads at a time.
+            }
+        free(temp);
+    } /* count_sort */
 
 int main(int argc, const char** argv) {
-    if argc != 3 {
+    if (argc != 3) {
         printf("Usage: %s <num_threads> <num_elements>\n", argv[0]);
         return 1;
     }
-    int num_threads = std::stoi(argv[1]);
-    int num_elements = std::stoi(argv[2]);
-    int * a = (int *) malloc(num_elements * sizeof(int));
+    //set the omp number of threads to use in this program.
+    omp_set_num_threads(std::stoi(argv[1]));
 
-    // Generate random integers in the range 1 to n (initialize with srand(100) ) 
-    // print the original sequence
-    // print the ordered sequence
-    // Your program should print all list elements on a single line and print each\
-     list on its own line. Additionally, label each list something like the following \
-     example output.
+    int num_elements = std::stoi(argv[2]);
+    int * a = static_cast<int *>(std::malloc(num_elements * sizeof(int)));
+
     srand(100);
-    for (int i = 0; i < num_elements; i++) {
-        a[i] = rand() % num_elements + 1;
-    }
+    //use omp, fill a with random numbers
+    #pragma omp parallel for shared(a) //the array is seeded, but the different\
+    threads will fill the array differently, but with the same numbers every time.
+        for (int i = 0; i < num_elements; i++) { //start of for loop
+            a[i] = rand() % num_elements + 1;    //fill the array with a thread counted value
+        }
+    
 
     printf("original: ");
+    for (int i = 0; i < num_elements; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    count_sort_thread_safe(a, num_elements);
+
+    printf("sorted: ");
     for (int i = 0; i < num_elements; i++) {
         printf("%d ", a[i]);
     }
@@ -55,17 +83,5 @@ int main(int argc, const char** argv) {
     return 0;
 }
 
-// 1. If we try to parallelize the for i loop (the outer loop),\
- which variables should be private and which should be shared? (5 points)
-    //Answer: the variables i, j, and count should be private while \
-    temp and 'a' should be shared
 
-// 2. If we consider the memcpy implementation not thread-safe,\
- how would you approach parallelizing this operation? (5 points)
-    //create a memory safe version of the function. Using a OMP critical section\
-     to ensure that only one thread can access the memory at a time will also\
-     allow additional safety.
-    
-// 3. Write a C/C++ OpenMP program that includes a parallel implementation\
- of count_sort. 30 points)
      
